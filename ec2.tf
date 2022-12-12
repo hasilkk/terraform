fb467f501ee12c145f2cb86817847368db5f7aba
hasilkk <downloadhkk@gmail.com>
2022-12-12 13:38:36 +0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,8 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_instance" "terraform-first-ec2" {
    ami = "ami-08c40ec9ead489470" 
   instance_type = "t2.micro"
