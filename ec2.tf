d5d5c5f98692f98722141cd5942144d53615d712
hasilkk <downloadhkk@gmail.com>
2022-12-12 13:28:19 +0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,7 @@
-resource "aws_instance" "terraform-first-ec2" { ami = "ami-08c40ec9ead489470" instance_type = "t2.micro"
-
-tags = { Name = "Hello-First-Terraform-EC2" }
+resource "aws_instance" "terraform-first-ec2" {
+   ami = "ami-08c40ec9ead489470" 
+  instance_type = "t2.micro"
+  tags = { 
+  Name = "Hello-First-Terraform-EC2" 
+  }
+}
